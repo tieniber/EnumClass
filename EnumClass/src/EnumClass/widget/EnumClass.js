52aa566bb8d9639e741a3d30188a582b71af130b
Eric Tieniber <tieniber@gmail.com>
2016-12-07 17:46:09 -0500

    Revert "merged"
    This reverts commit 8111ece7a2844aa64028fa6240b4dd0fd01210ae, reversing
    changes made to 2168d7ceece63c7b09eb17720a900931823f511a.

--- EnumClass/src/EnumClass/widget/EnumClass.js
+++ EnumClass/src/EnumClass/widget/EnumClass.js
@@ -1,142 +1,231 @@
 define([
-	"dojo/_base/declare",
-  "mxui/widget/_WidgetBase",
-  'dojo/_base/lang',
-  "dojo/dom-construct",
-  "dojo/dom-attr",
-  "dojo/NodeList-traverse"
-], function (declare, _WidgetBase, lang, domConstruct, domAttr) {
-	"use strict";
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    'dojo/_base/lang',
+    "dojo/dom-construct",
+    "dojo/dom-attr",
+    "dojo/dom-class",
+    "dojo/NodeList-traverse"
+], function(declare, _WidgetBase, lang, domConstruct, domAttr, domClass) {
+    "use strict";
 
-	// Declare widget"s prototype.
-	return declare("EnumClass.widget.EnumClass", [_WidgetBase], {
+    // Declare widget"s prototype.
+    return declare("EnumClass.widget.EnumClass", [_WidgetBase], {
 
-    // Set in Modeler
-    name : "",
-    enumvalues : [], //value array, keep it compatible with mx4
-    glyphicon: "",
-    applyToEnum: "",
-    associationClassName: "",
+        // Set in Modeler
+        name: "",
+        enumvalues: [], //value array, keep it compatible with mx4
+        glyphicon: "",
+        applyToEnum: "",
+        associationClassName: "",
 
-    // internal variables
-    contextGUID : null,
-    caption : [],
-    classnames : [],
-    replacements : [],
-    curindex : 0,
-    element : null,
-    attrHandle: null,
-    objHandle: null,
-    defaultClass: "",
-    elementToApplyTo: null,
-	showWidget: true,
-	referenceEntity: null,
-	_referenceName: null,
-	attributeType: "primitive",
+        // internal variables
+        contextGUID: null,
+        caption: [],
+        classnames: [],
+        replacements: [],
+        curindex: 0,
+        element: null,
+        attrHandle: null,
+        objHandle: null,
+        defaultClass: "",
+        elementToApplyTo: null,
+        showWidget: true,
+        referenceEntity: null,
+        _referenceName: null,
+        attributeType: "primitive",
 
-    postCreate : function () {
+        postCreate: function() {
 
-		//Polyfill so we can use element.closest in IE
-		// matches polyfill
-		window.Element && function(ElementPrototype) {
-		    ElementPrototype.matches = ElementPrototype.matches ||
-		    ElementPrototype.matchesSelector ||
-		    ElementPrototype.webkitMatchesSelector ||
-		    ElementPrototype.msMatchesSelector ||
-		    function(selector) {
-		        var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
-		        while (nodes[++i] && nodes[i] != node);
-		        return !!nodes[i];
-		    }
-		}(Element.prototype);
+            //Polyfill so we can use element.closest in IE
+            // matches polyfill
+            window.Element && function(ElementPrototype) {
+                ElementPrototype.matches = ElementPrototype.matches || ElementPrototype.matchesSelector || ElementPrototype.webkitMatchesSelector || ElementPrototype.msMatchesSelector || function(selector) {
+                    var node = this,
+                        nodes = (node.parentNode || node.document).querySelectorAll(selector),
+                        i = -1;
+                    while (nodes[++i] && nodes[i] != node)
+                    ;
+                    return !!nodes[i];
+                }
+            }(Element.prototype);
 
-		// closest polyfill
-		window.Element && function(ElementPrototype) {
-		    ElementPrototype.closest = ElementPrototype.closest ||
-		    function(selector) {
-		        var el = this;
-		        while (el.matches && !el.matches(selector)) el = el.parentNode;
-		        return el.matches ? el : null;
-		    }
-		}(Element.prototype);
+            // closest polyfill
+            window.Element && function(ElementPrototype) {
+                ElementPrototype.closest = ElementPrototype.closest || function(selector) {
+                    var el = this;
+                    while (el.matches && !el.matches(selector))
+                        el = el.parentNode;
+                    return el.matches
+                        ? el
+                        : null;
+                }
+            }(Element.prototype);
 
-		//End polyfill
+            //End polyfill
 
-    // STYLE BY REFERENCE
-		if (this.referenceEntity) {
-			this._referenceName = this.referenceEntity.split("/")[0];
-		}
+            // STYLE BY REFERENCE
+            if (this.referenceEntity) {
+                this._referenceName = this.referenceEntity.split("/")[0];
+            }
 
-		this.caption = [];
-		this.classnames = [];
-		this.replacements = [];
-		// copy data from object array
-		for (var i = 0; i < this.enumvalues.length; i++) {
-			this.caption.push(this.enumvalues[i].captions);
-			this.classnames.push(this.enumvalues[i].classnames);
-			this.replacements.push(this.enumvalues[i].replacements);
-		}
+            this.caption = [];
+            this.classnames = [];
+            this.replacements = [];
+            // copy data from object array
+            for (var i = 0; i < this.enumvalues.length; i++) {
+                this.caption.push(this.enumvalues[i].captions);
+                this.classnames.push(this.enumvalues[i].classnames);
+                this.replacements.push(this.enumvalues[i].replacements);
+            }
 
-		this.element = domConstruct.create("span");
-		this.domNode.appendChild(this.element);
+            this.element = domConstruct.create("span");
+            this.domNode.appendChild(this.element);
 
-		if (!this.showWidget) {
-			domAttr.set(this.element, "style", "display:none;");
-		}
+            if (!this.showWidget) {
+                domAttr.set(this.element, "style", "display:none;");
+            }
 
-		switch (this.applyToEnum) { //Select the right element to apply the class too
-		case "SELF":
-			this.elementToApplyTo = this.element;
-			break;
-		case "ROW":
-			this.elementToApplyTo = this.element.closest(".mx-templategrid-row");
-			break;
-		case "LISTITEM":
-			this.elementToApplyTo = this.element.closest(".mx-listview-item");
-			break;
-		case "PARENT":
-			this.elementToApplyTo = this.domNode.parentElement;
-			break;
-		case "SIBLING":
-			this.elementToApplyTo = this.domNode.previousSibling;
-			break;
-		default:
-			this.elementToApplyTo = this.element;
-		}
-		this.defaultClass = domAttr.get(this.elementToApplyTo, "class");
+            switch (this.applyToEnum) { //Select the right element to apply the class too
+                case "SELF":
+                    this.elementToApplyTo = this.element;
+                    break;
+                case "ROW":
+                    this.elementToApplyTo = this.element.closest(".mx-templategrid-row");
+                    break;
+                case "PARENT":
+                    this.elementToApplyTo = this.domNode.parentElement;
+                    break;
+                case "SIBLING":
+                    this.elementToApplyTo = this.domNode.previousSibling;
+                    break;
+                default:
+                    this.elementToApplyTo = this.element;
+            }
+            this.defaultClass = domAttr.get(this.elementToApplyTo, "class");
 
-		if (this.defaultClass !== "") {
-			this.defaultClass += " ";
-		}
-    },
+            if (this.defaultClass !== "") {
+                this.defaultClass += " ";
+            }
+        },
 
-    update : function (obj, callback) {
-      if (obj) {
-        this.contextGUID = obj.getGuid();
-        // check reference
-        if (this._referenceName && obj.get(this._referenceName) !== ""){
-          // console.log(obj.get(this._referenceName))
-          // set the classes
-          this.elementToApplyTo.className += " " + this.associationClassName
-        }
-        this._resetSubscriptions();
-      }
-      callback();
-    },
+        update: function(obj, callback) {
+            if (obj) {
+                this.contextGUID = obj.getGuid();
+                this._contextObj = obj;
+                // check reference
+                this._updateRendering();
+                this._resetSubscriptions();
+            }
+            callback();
+        },
 
-    _setValueAttr : function (value) {
-		if(this.attributeType == "reference") {
-			if(value) {
-				value = "true";
-			} else {
-				value = "false";
-			}
-		}
+        _setValueAttr: function(value) {
+            if (this.attributeType == "reference") {
+                if (value) {
+                    value = "true";
+                } else {
+                    value = "false";
+                }
+            }
 
-		if(value === true) {
-        	value = "true";
-        } else if (value === false) {
-        	value = "false";
+            if (value === true) {
+                value = "true";
+            } else if (value === false) {
+                value = "false";
+            }
+
+            var i = this.caption.indexOf(value);
+            var classname = "";
+            var toDisplay = "";
+
+            if ((i >= 0) && (i < this.caption.length)) {
+                this.curindex = i;
+                classname = this.classnames[i];
+            } else {
+                this.curindex = 0;
+                classname = "";
+            }
+
+            if (this.replacements[i] !== "" && typeof this.replacements[i] !== "undefined") {
+                toDisplay = this.replacements[i];
+            } else {
+                toDisplay = value;
+            }
+
+            if (this.glyphicon !== "") {
+                classname = classname + " glyphicon glyphicon-" + this.glyphicon + " ";
+            }
+            domAttr.set(this.elementToApplyTo, "class", this.defaultClass + classname); //Set the class to the existing class + enum-determined-class
+
+            if (this.glyphicon !== "") {
+                domAttr.set(this.element, "innerHTML", "");
+                domAttr.set(this.element, "title", toDisplay); //Set innerHTML empty and tooltip to caption
+            } else {
+                domAttr.set(this.element, "innerHTML", toDisplay); //Set the innerHTML to the value of the attribute
+            }
+        },
+
+        _getValueAttr: function(value) {
+            return this.caption[this.curindex];
+        },
+
+        //stub function, will be used or replaced by the client environment
+        onChange: function() {},
+
+        textChange: function(e) {
+            this.onChange();
+        },
+
+        _unsubscribe: function() {
+            if (this.attrHandle) {
+                mx.data.unsubscribe(this.attrHandle);
+            }
+            if (this.objHandle) {
+                mx.data.unsubscribe(this.objHandle);
+            }
+        },
+
+        _resetSubscriptions: function() {
+            var attributeName = this.name;
+            if (this.attributeType == "reference") {
+                attributeName = this._referenceName;
+            }
+
+            if (this.contextGUID) {
+                this._unsubscribe();
+
+                // subscription for object refresh
+                this.objHandle = this.subscribe({
+                    guid: this.contextGUID,
+                    callback: lang.hitch(this, function() {
+                        this._updateRendering();
+                    })
+                });
+
+                this.attrHandle = this.subscribe({
+                    guid: this.contextGUID,
+                    attr: this.name,
+                    callback: lang.hitch(this, function(guid, attr, attrValue) {
+                        this._setValueAttr(attrValue);
+                    })
+                });
+            }
+        },
+
+        uninitialize: function() {
+            this._unsubscribe();
+        },
+
+        _updateRendering: function() {
+          if (this._referenceName && this._contextObj.get(this._referenceName) !== "") {
+              // set the classes
+              // this.elementToApplyTo.className += " " + this.associationClassName
+              domClass.add(this.elementToApplyTo, this.associationClassName)
+          }
+          else {
+            domClass.remove(this.elementToApplyTo, this.associationClassName)
+          }
         }
 
     });
